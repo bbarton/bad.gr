ebbeb40298418cef175d34ebc530c4a25c43eda8
Chris Bolton <c@chrisbolton.me>
2012-11-01 16:42:53 -0700

    :P

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 
   if (process.env.REDISTOGO_URL) {
     rtg = require('url').parse(process.env.REDISTOGO_URL);
-    require('redis').createClient(rtg.port, rtg.hostname);
+    redis = require('redis').createClient(rtg.port, rtg.hostname);
     redis.auth(rtg.auth.split(':')[1]);
   } else {
     redis = require('redis').createClient();
